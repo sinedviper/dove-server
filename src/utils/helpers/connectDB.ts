7e4356daf18b3e4f34ec0b53da1ce108115b4532
thewoodman999 <119786879+thewoodman999@users.noreply.github.com>
2022-12-04 13:00:56 +0200

    fix bugs

--- src/utils/helpers/connectDB.ts
+++ src/utils/helpers/connectDB.ts
@@ -11,8 +11,8 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
+  host: process.env.HOST,
+  port: Number(process.env.PORT_DATABASE),
   username: process.env.ADMIN,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
